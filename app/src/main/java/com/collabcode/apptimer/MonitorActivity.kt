789439caab86a3b3aece0de77f78f9d5a3636be3
prabhatsn1 <prabhatsn1@gmail.com>
2020-12-02 16:10:06 +0530

    3 Activity Complete

---
+++ app/src/main/java/com/collabcode/apptimer/MonitorActivity.kt
@@ -0,0 +1,11 @@
+package com.collabcode.apptimer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MonitorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_monitor)
+    }
+}
--- app/src/main/java/com/collabcode/apptimer/UsageGoalActivity.kt
+++ app/src/main/java/com/collabcode/apptimer/UsageGoalActivity.kt
@@ -1,11 +1,19 @@
 package com.collabcode.apptimer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class UsageGoalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_usage_goal)
+        val doneBtn = findViewById<Button>(R.id.doneBtn)
+        doneBtn.setOnClickListener(View.OnClickListener {
+            val mActivity = Intent(this,MonitorActivity::class.java)
+            startActivity(mActivity)
+        })
     }
 }
