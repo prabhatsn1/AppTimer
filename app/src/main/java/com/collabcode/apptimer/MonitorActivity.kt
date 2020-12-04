c67210c92d43a2d8e182f12acbb04e0d44990be5
prabhatsn1 <prabhatsn1@gmail.com>
2020-12-04 15:18:00 +0530

    Dashboard Activity

--- app/src/main/java/com/collabcode/apptimer/MonitorActivity.kt
+++ app/src/main/java/com/collabcode/apptimer/MonitorActivity.kt
@@ -1,11 +1,26 @@
 package com.collabcode.apptimer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MonitorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_monitor)
+
+        val nextBtn = findViewById<Button>(R.id.nextBtn)
+        val backBtn = findViewById<Button>(R.id.backBtn)
+
+        nextBtn.setOnClickListener(View.OnClickListener {
+            val mActivity = Intent(this,Dashboard::class.java)
+            startActivity(mActivity)
+        })
+        backBtn.setOnClickListener(View.OnClickListener {
+            val nActivity = Intent(this,UsageGoalActivity::class.java)
+            startActivity(nActivity)
+        })
     }
 }
